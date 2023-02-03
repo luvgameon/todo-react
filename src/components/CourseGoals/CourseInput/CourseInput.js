cd5e038630f09152b8870865ef5757bcea8f2c86
luvgameon <luvgameon@gmail.com>
2023-02-04 00:58:08 +0530

    fade add more btn

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,23 +5,32 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isvalid, setisvalid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length>0){
+      setisvalid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length===0)
+    {
+      setisvalid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isvalid?'invalid':''}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button type="submit" valid={isvalid}>Add Goal</Button>
     </form>
   );
 };
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,9 +3,16 @@ import React from 'react';
 import './Button.css';
 
 const Button = props => {
+  // const [valid, setvalid] = useState(false);
+  // if(props.valid){
+  //   setvalid(true);
+  //   return;
+  // }
+ 
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} style={{background:`${props.valid?'#8b005d':'#eacbe0'}`}} className='button' onClick={props.onClick}>
       {props.children}
+      
     </button>
   );
 };
